c19198e4f6416a4d6fe9b0b830b6fed0a5796e3b
Yair Levi <yairle@microsoft.com>
2016-06-23 13:05:54 +0300

    Adding saving unique subtitles for unique video id

--- public/core.js
+++ public/core.js
@@ -24,8 +24,20 @@ app.controller('subtitleTableController',function subtitleTableController($scope
   	}
   	$scope.speed = 1.0;
   	$scope.currentIndex = 0;
-	$scope.userId = "FakeUser";
-	$scope.videoId = "FakeVideo";
+
+  	$scope.getQueryVariable = function(variable) {
+	    var query = window.location.search.substring(1);
+	    var vars = query.split('&');
+	    for (var i = 0; i < vars.length; i++) {
+	        var pair = vars[i].split('=');
+	        if (decodeURIComponent(pair[0]) == variable) {
+	            return decodeURIComponent(pair[1]);
+	        }
+	    }
+	}
+
+	$scope.userId = $scope.getQueryVariable("userId");
+	$scope.videoId = $scope.getQueryVariable("id");
 
 	$scope.subtitles = [];
 
